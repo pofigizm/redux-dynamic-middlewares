813f201c4682f02856aae91a62aceec88bb97752
Denis Iogansen <pofigizm@gmail.com>
2017-10-29 22:31:19 +0300

    add tests

---
+++ src/index.test.js
@@ -0,0 +1,108 @@
+import { applyMiddleware, createStore } from 'redux'
+import dynamicMiddlewares, { addMiddleware, removeMiddleware, resetMiddlewares } from './'
+
+const reducer = (state = {}, action) => {
+  if (action.type === 'foo') return { foo: 'bar' }
+  return state
+}
+
+test('redux should work without error', () => {
+  console.error = jest.fn()
+  const store = createStore(reducer, applyMiddleware(dynamicMiddlewares))
+  expect(store.getState()).toEqual({})
+  store.dispatch({ type: 'foo' })
+  expect(console.error).not.toBeCalled()
+  expect(store.getState()).toEqual({ foo: 'bar' })
+})
+
+test('middleware should be called', () => {
+  const store = createStore(reducer, applyMiddleware(dynamicMiddlewares))
+  const middlewareWork = jest.fn()
+  const middleware = store => next => action => {
+    middlewareWork(action)
+    return next(action)
+  }
+  addMiddleware(middleware)
+
+  store.dispatch({ type: 'foo' })
+  expect(middlewareWork).toBeCalledWith({ type: 'foo' })
+})
+
+test('all middlewares by single add should be called', () => {
+  const store = createStore(reducer, applyMiddleware(dynamicMiddlewares))
+  const firstMiddlewareWork = jest.fn()
+  const firstMiddleware = store => next => action => {
+    firstMiddlewareWork(action)
+    return next(action)
+  }
+  const secondMiddlewareWork = jest.fn()
+  const secondMiddleware = store => next => action => {
+    secondMiddlewareWork(action)
+    return next(action)
+  }
+  addMiddleware(firstMiddleware, secondMiddleware)
+
+  store.dispatch({ type: 'foo' })
+  expect(firstMiddlewareWork).toBeCalledWith({ type: 'foo' })
+  expect(secondMiddlewareWork).toBeCalledWith({ type: 'foo' })
+})
+
+test('all middlewares by separate add should be called', () => {
+  const store = createStore(reducer, applyMiddleware(dynamicMiddlewares))
+  const firstMiddlewareWork = jest.fn()
+  const firstMiddleware = store => next => action => {
+    firstMiddlewareWork(action)
+    return next(action)
+  }
+  const secondMiddlewareWork = jest.fn()
+  const secondMiddleware = store => next => action => {
+    secondMiddlewareWork(action)
+    return next(action)
+  }
+  addMiddleware(firstMiddleware)
+  addMiddleware(secondMiddleware)
+
+  store.dispatch({ type: 'foo' })
+  expect(firstMiddlewareWork).toBeCalledWith({ type: 'foo' })
+  expect(secondMiddlewareWork).toBeCalledWith({ type: 'foo' })
+})
+
+test('removed middlewares should not be called', () => {
+  const store = createStore(reducer, applyMiddleware(dynamicMiddlewares))
+  const firstMiddlewareWork = jest.fn()
+  const firstMiddleware = store => next => action => {
+    firstMiddlewareWork(action)
+    return next(action)
+  }
+  const secondMiddlewareWork = jest.fn()
+  const secondMiddleware = store => next => action => {
+    secondMiddlewareWork(action)
+    return next(action)
+  }
+  addMiddleware(firstMiddleware, secondMiddleware)
+  removeMiddleware(secondMiddleware)
+
+  store.dispatch({ type: 'foo' })
+  expect(firstMiddlewareWork).toBeCalledWith({ type: 'foo' })
+  expect(secondMiddlewareWork).not.toBeCalled()
+})
+
+test('reset middlewares should work', () => {
+  const store = createStore(reducer, applyMiddleware(dynamicMiddlewares))
+  const firstMiddlewareWork = jest.fn()
+  const firstMiddleware = store => next => action => {
+    firstMiddlewareWork(action)
+    return next(action)
+  }
+  const secondMiddlewareWork = jest.fn()
+  const secondMiddleware = store => next => action => {
+    secondMiddlewareWork(action)
+    return next(action)
+  }
+  addMiddleware(firstMiddleware, secondMiddleware)
+  resetMiddlewares()
+
+  store.dispatch({ type: 'foo' })
+  expect(firstMiddlewareWork).not.toBeCalled()
+  expect(secondMiddlewareWork).not.toBeCalled()
+})
