b81be4d5bc570412f2afa11e278fa3b480c643d3
Denis Iogansen <pofigizm@gmail.com>
2018-08-28 21:28:14 +0300

    possibility use many instances [#5]

--- src/index.js
+++ src/index.js
@@ -1,37 +1,53 @@
 import { compose } from 'redux'
 
-let allDynamicMiddlewares = []
+const createDynamicMiddlewares = () => {
+  let allDynamicMiddlewares = []
 
-const dynamicMiddlewares = store => next => (action) => {
-  const chain = allDynamicMiddlewares.map(middleware => middleware(store))
+  const enhancer = store => next => (action) => {
+    const chain = allDynamicMiddlewares.map(middleware => middleware(store))
 
-  return compose(...chain)(next)(action)
-}
-
-const addMiddleware = (...middlewares) => {
-  allDynamicMiddlewares = [...allDynamicMiddlewares, ...middlewares]
-}
-
-const removeMiddleware = (middleware) => {
-  const index = allDynamicMiddlewares.findIndex(d => d === middleware)
-
-  if (index === -1) {
-    // eslint-disable-next-line no-console
-    console.error('Middleware does not exist!', middleware)
-
-    return
+    return compose(...chain)(next)(action)
   }
 
-  allDynamicMiddlewares = allDynamicMiddlewares.filter((_, mdwIndex) => mdwIndex !== index)
+  const addMiddleware = (...middlewares) => {
+    allDynamicMiddlewares = [...allDynamicMiddlewares, ...middlewares]
+  }
+
+  const removeMiddleware = (middleware) => {
+    const index = allDynamicMiddlewares.findIndex(d => d === middleware)
+
+    if (index === -1) {
+      // eslint-disable-next-line no-console
+      console.error('Middleware does not exist!', middleware)
+
+      return
+    }
+
+    allDynamicMiddlewares = allDynamicMiddlewares.filter((_, mdwIndex) => mdwIndex !== index)
+  }
+
+  const resetMiddlewares = () => {
+    allDynamicMiddlewares = []
+  }
+
+  return {
+    enhancer,
+    addMiddleware,
+    removeMiddleware,
+    resetMiddlewares,
+  }
 }
 
-const resetMiddlewares = () => {
-  allDynamicMiddlewares = []
-}
+const dynamicMiddlewaresInstance = createDynamicMiddlewares()
 
-export default dynamicMiddlewares
-export {
+export default dynamicMiddlewaresInstance.enhancer
+
+export const {
   addMiddleware,
   removeMiddleware,
   resetMiddlewares,
+} = dynamicMiddlewaresInstance
+
+export {
+  createDynamicMiddlewares,
 }
